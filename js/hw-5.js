5b06b7cae944e6bc349522d3f88807ba6b6153a5
Антонюк Евгений Викторович <96491514+evgenijantonyuk@users.noreply.github.com>
2026-09-04 12:32:56 +0700

    Arrays

--- js/hw-5.js
+++ js/hw-5.js
@@ -1,1 +1,34 @@
-alert('hello')
+// Массивы
+const numbers = [7, 2, 3, 4, 5]
+
+for (i = 0 ; i < numbers.length; i++) {
+      if(numbers[i] === 3) break
+      console.log(numbers[i])
+}
+
+numbers.forEach((el, index) => {
+      el += 1
+      console.log(`${index}: ${el}`)
+})
+
+let text = 'Привет как дела?'
+console.log(text)
+
+textSplit = text.split(' ')
+console.log(textSplit)
+
+joinText = textSplit.join('*/*')
+console.log(joinText)
+
+console.log('Массив мутирует')
+const numbersSort = [ 2, 5, 3, 1, 7, 2 ]
+numbersSort.sort((a, b) => a - b)
+console.log(numbersSort)
+
+// Без мутации оригинального массива
+const original = [3, 1, 2]
+const sorted = original.toSorted((a, b) => a - b)
+console.log('Без мутации оригинального массива');
+console.log(original); // [3, 1, 2] (не изменился)
+console.log(sorted);   // [1, 2, 3]
+
