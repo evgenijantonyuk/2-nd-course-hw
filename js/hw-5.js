8a3bef160c25b7790c77fefec9fb7aa6a16085eb
Антонюк Евгений Викторович <96491514+evgenijantonyuk@users.noreply.github.com>
2026-09-07 19:05:13 +0700

    add Task 3

--- js/hw-5.js
+++ js/hw-5.js
@@ -1,4 +1,47 @@
-// Task 1. Game quiz (Викторина)
+
+// Task 1
+const arrayTask1 = [1, 5, 4, 10, 0, 3]
+// Цикл For
+      for (i = 0; i < arrayTask1.length; i++) {
+            console.log(`Цикл до искомой цифры - ${arrayTask1[i]}`)
+            if(arrayTask1[i] === 10) {
+                  console.log(`Искомая цифра - ${arrayTask1[i]}`)
+                  break
+            }
+      }
+// Цикл For of
+      for (const number of arrayTask1) {
+            console.log(`Цикл до искомой цифры - ${number}`)
+            if(number === 10)
+            break
+      }
+
+// Task 2
+const arrayTask2 = [1, 5, 4, 10, 0, 3]
+console.log(`Массив ${arrayTask2}`)
+
+const index = arrayTask2.indexOf(4)
+console.log(`${index} - индекс числа 4`)
+
+// Task 3
+const arrayTask3 = [1, 3, 5, 10, 20]
+const joinArrayTask3 = arrayTask3.join(', ')
+console.log(joinArrayTask3)
+
+// Task 4
+// Task 5
+// Task 6
+// Task 7
+// Task 8
+// Task 9
+// Task 10
+// Task 11
+// Task 12
+// Task 13
+// Task 14
+// Task 15
+
+//  Game quiz (Викторина)
 const startQuiz = () => {
   const quiz = [
     {
@@ -40,5 +83,4 @@ const startQuiz = () => {
     `Викторина окончена! Ваш результат: ${correctAnswersCount} из ${quiz.length} правильных ответов.`,
   );
 };
-
-// Task 2
+// =======================
